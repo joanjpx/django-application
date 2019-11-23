27f961f15666d3cc0c28a17d58b7c0eee5c888f1
Joan P. Alvarado <joansyn.s@gmail.com>
2019-11-23 10:16:29 -0500

    Testing HTML on HttpResponse Object Class

--- django_application/views.py
+++ django_application/views.py
@@ -4,4 +4,4 @@ def saludo(request):
     return HttpResponse("Hello World from Django")
 
 def despedida(request):
-    return HttpResponse("Goodbye from Django")
+    return HttpResponse("<h1>Goodbye from Django</h1>")
