523a44a3bf1025a626d3c54879623823b2b99ef2
Joan P. Alvarado <joansyn.s@gmail.com>
2019-11-27 00:29:13 -0500

    getAge Method @ views.py

--- django_application/settings.py
+++ django_application/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'django_application'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+
+CORS_ALLOW_CREDENTIALS = True
--- django_application/views.py
+++ django_application/views.py
@@ -1,7 +1,27 @@
-from django.http import *
+from django.http import HttpResponse
+import datetime
 
 def saludo(request):
     return HttpResponse("Hello World from Django")
 
 def despedida(request):
-    return HttpResponse("<h1>Goodbye from Django</h1>")
+    
+    HTML = """<body>
+        <div>
+            <h1>GoodBye From Django</h1>
+        </div>
+    </body>"""
+
+    return HttpResponse(HTML)
+
+def getDate(request):
+    now = datetime.datetime.now()
+    return HttpResponse(now)
+
+def calculateAge(request, year):
+    #return HttpResponse(year)
+    edadActual = 24
+    periodo = year-2019
+    edadFutura = edadActual+periodo
+    html = "<h2>En el año %s tendrás %s años</h2>" %(year,edadFutura)
+    return HttpResponse(html)
